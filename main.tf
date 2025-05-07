deb1f4579c4b49ce4eea53490b2575b35bb26da5
Ihor Holinei <ihorholinei@MacBook-Air.local>
2025-05-07 13:07:09 -0500

    comment out s3bucket

--- main.tf
+++ main.tf
@@ -11,23 +11,19 @@ resource "aws_vpc" "test-vpc" {
 }
 
 resource "aws_subnet" "test-subnet" {
-  vpc_id     = aws_vpc.test-vpc.id
+  vpc_id = aws_vpc.test-vpc.id
   cidr_block = "10.0.1.0/25"
 }
 
 resource "aws_instance" "test-instance" {
-  ami           = "ami-058a8a5ab36292159"
+  ami = "ami-058a8a5ab36292159"
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.test-subnet.id
+  subnet_id = aws_subnet.test-subnet.id
 
   tags = {
-    Name = "gh-action-instance"
+    Name = "gh-action-instance-test"
   }
 }
 
 
 
-resource "aws_s3_bucket" "example" {
-  bucket = "my-gh-actions-bucket-example"
-
-}
